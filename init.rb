9a82207a59606fa66f13122788596039e90e7ff9
Daniela <d.kuster@ablida.de>
2021-01-25 14:34:36 +0100

    initial commit

---
+++ init.rb
@@ -0,0 +1,14 @@
+require 'locale_integer_delimiter_patch'
+
+Redmine::Plugin.register :locale_integer_delimiter do
+  name 'Locale Integer Delimiter plugin'
+  author 'bokos'
+  description 'Displays integers with delimiter from selected locale'
+  version '0.0.1'
+  url 'https://github.com/bokos/locale_integer_delimiter'
+  author_url 'https://github.com/bokos/locale_integer_delimiter'
+
+  RedmineApp::Application.config.after_initialize do
+    ApplicationHelper.send(:include, LocaleIntegerDelimiterPatch)
+  end
+end
---
+++ lib/locale_integer_delimiter_patch.rb
@@ -0,0 +1,17 @@
+module LocaleIntegerDelimiterPatch
+  def self.included(base)
+    def format_object_with_locale_delimiter(object, html=true, &block)
+      case object.class.name
+      when 'Integer'
+        number_with_delimiter(object)
+      else
+        format_object_without_locale_delimiter(object, html, &block)
+      end
+    end
+
+    base.class_eval do
+      alias_method :format_object_without_locale_delimiter, :format_object
+      alias_method :format_object, :format_object_with_locale_delimiter
+    end
+  end
+end
